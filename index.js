baacce5191be179fb0328f93fcc42d7c457b362a
amandasuzzanne <amandasuzanne12@gmail.com>
2024-04-08 10:08:58 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'nadine';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'hope';
+  return leastFavoriteCustomer;
+}
